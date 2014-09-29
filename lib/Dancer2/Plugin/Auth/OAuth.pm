a68cbeca9899155f4bd801355828f75262857072
B10m <menno@b10m.net>
2014-09-29 09:10:42 +0200

    initial commit

---
+++ lib/Dancer2/Plugin/Auth/OAuth.pm
@@ -0,0 +1,39 @@
+package Dancer2::Plugin::Auth::OAuth;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dancer2::Plugin::Auth::OAuth - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Dancer2::Plugin::Auth::OAuth;
+
+=head1 DESCRIPTION
+
+Dancer2::Plugin::Auth::OAuth is
+
+=head1 AUTHOR
+
+Menno Blom E<lt>blom@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Menno Blom
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
