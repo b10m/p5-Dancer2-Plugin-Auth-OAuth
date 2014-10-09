f8cf5ab196eac694fa3c7e632f08121d8949c9b2
B10m <menno@b10m.net>
2014-10-09 08:51:37 +0200

    documentation

--- lib/Dancer2/Plugin/Auth/OAuth.pm
+++ lib/Dancer2/Plugin/Auth/OAuth.pm
@@ -78,9 +78,12 @@ authentication easy.
 The module is highly influenced by L<Plack::Middleware::OAuth> and Dancer 1
 OAuth modules, but unlike the Dancer 1 versions, this plugin only needs
 configuration (look mom, no code needed!). It automatically sets up the
-needed routes (defaults to '/auth/<provider>' and '/auth/<provider>/callback'.
+needed routes (defaults to C</auth/$provider> and C</auth/$provider/callback>).
+So if you define the Twitter provider in your config, you should automatically
+get C</auth/twitter> and C</auth/twitter/callback>.
 
-After a successful OAuth dance, the user info is stored in the session.
+After a successful OAuth dance, the user info is stored in the session. What
+you do with it afterwards is up to you.
 
 =head1 CONFIGURATION
 
